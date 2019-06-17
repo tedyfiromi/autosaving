e87ffedb1c8fdd95c44b05671d6ecadac324b94a
n894779 <lucio.passos@santander.com.br>
2019-06-17 18:22:50 -0300

    refactory in GoalController and TransactionGoalController

--- autosaving-goal/src/main/java/com/santander/autosavings/middleware/repository/GoalRepository.java
+++ autosaving-goal/src/main/java/com/santander/autosavings/middleware/repository/GoalRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.santander.autosavings.middleware.model.Goal;
-import com.santander.autosavings.middleware.model.TransactionGoal;
 
 @Repository
 public interface GoalRepository extends MongoRepository<Goal, String>{
